333791d3cadbe9038ad68813b92471107c70c072
dengwuxue <1654057048@qq.com>
2020-06-24 20:33:28 +0800

    提交第一次

---
+++ src/main/java/com/anjubao/firstpro/One.java
@@ -0,0 +1,11 @@
+package com.anjubao.firstpro;
+
+public class One {
+    public int add(int a,int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+    new  One().add(1,2);
+    }
+}
---
+++ src/main/java/com/anjubao/firstpro/entity/User.java
@@ -0,0 +1,7 @@
+package com.anjubao.firstpro.entity;
+
+public class User {
+    private String name;
+    private String sex;
+    private int age;
+}
